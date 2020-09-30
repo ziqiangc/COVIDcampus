c11747012e2f1b43311ebb54e60b8acb5e53277e
Ziqiang Chen <ziqiangc@buffalo.edu>
2020-09-30 13:02:56 -0400

    Use confidence interval by 1.96*se from fitted model

--- Poisson Model.R
+++ Poisson Model.R
@@ -6,6 +6,8 @@ dat <- data.frame(time=c(1:7),
 
 lod.model <- function(dat, cutoff=100) {
 
+  dat <- dat[complete.cases(dat),]
+  
   # Must have at least 3 datapoints at different times
   if (length(unique(dat$positive)) < 3) stop("too few data points to fit curve")
   
@@ -17,37 +19,37 @@ lod.model <- function(dat, cutoff=100) {
   preds <- predict(mod, newdata=data.frame(time=grid), type="link", se.fit=TRUE)
   
   # confidence band
-  # se <- 1.96 * preds$se.fit
-  # upr <- unlist(mod$family$linkinv(preds$fit + se))
-  # lwr <- unlist(mod$family$linkinv(preds$fit - se))
-  # fit <- unlist(mod$family$linkinv(preds$fit))
+  se <- 1.96 * preds$se.fit
+  upr <- unlist(mod$family$linkinv(preds$fit + se))
+  lwr <- unlist(mod$family$linkinv(preds$fit - se))
+  fit <- unlist(mod$family$linkinv(preds$fit))
   
   param <- coef(mod)
   param.lwr <- confint(mod)[,1]
   param.upr <- confint(mod)[,2]
   
   
-  inverse.predict <- function(y, param) {
-    x = (log(y)-param[1]) / param[2]
-    return(x)
-  }
+  # inverse.predict <- function(y, param) {
+  #   x = (log(y)-param[1]) / param[2]
+  #   return(x)
+  # }
+  # 
+  # predict.fun <- function(x, param) {
+  #   y = exp(param[1] + param[2]*x)
+  # }
   
-  predict.fun <- function(x, param) {
-    y = exp(param[1] + param[2]*x)
-  }
-  
-  fit = predict.fun(grid, param)
-  upr = predict.fun(grid, param.lwr)
-  lwr = predict.fun(grid, param.upr)
+  # fit = predict.fun(grid, param)
+  # upr = predict.fun(grid, param.lwr)
+  # lwr = predict.fun(grid, param.upr)
   
   time.set <- (mod$family$linkfun(cutoff)-param[1]) / param[2] # 95% LOD Estimate
-  time.lwr <- (mod$family$linkfun(cutoff)-param.lwr[1]) / param.lwr[2]
-  time.upr <- (mod$family$linkfun(cutoff)-param.upr[1]) / param.upr[2]
+  # time.lwr <- (mod$family$linkfun(cutoff)-param.lwr[1]) / param.lwr[2]
+  # time.upr <- (mod$family$linkfun(cutoff)-param.upr[1]) / param.upr[2]
   
   
   ## Plot
   with(dat, plot(time, positive,ylab="Count of positives", xlab="Time period", pch=19, 
-                 xlim = c(1,max(10, dat$time*2, time.upr,time.lwr)), ylim=c(0,max(c(upr,lwr)))))
+                 xlim = c(1,max(10, sum(!is.na(dat$positive))*2)), ylim=c(0,max(c(upr,lwr)))))
   title(main=paste("Poisson Model, Time Threshold =", round(time.set, 3)))
   
   lines(grid, fit)
@@ -57,8 +59,8 @@ lod.model <- function(dat, cutoff=100) {
   
   abline(v=time.set, lty=2, col="red")
   
-  abline(v=time.upr, lty=3, col="royalblue")
-  abline(v=time.lwr, lty=3, col="royalblue")
+  # abline(v=time.upr, lty=3, col="royalblue")
+  # abline(v=time.lwr, lty=3, col="royalblue")
   
   
   axis(2, cutoff, labels = cutoff, col.axis="red", cex.axis=0.8)
